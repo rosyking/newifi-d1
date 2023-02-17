a033d64b5040c869670aadb8232799677e9bcfaf
rosyking <irosyking@gmail.com>
2023-02-17 22:01:08 +0800

    update repo

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,11 +8,6 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
-
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
-
-sed -i "/helloworld/d" "feeds.conf.default"
+sed -i '/helloworld/d' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
