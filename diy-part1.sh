16ff6c4124cd7172470d62b2a7860c34e8e4f215
rosyking <irosyking@gmail.com>
2023-02-18 12:41:32 +0800

    update workflow

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+# delete helloworld feed source
 sed -i '/helloworld/d' feeds.conf.default
+# Add two feed sources
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
