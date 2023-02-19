936547f341b52f9cd531d2b3b19d5a136ca50cbd
rosyking <irosyking@gmail.com>
2023-02-19 16:49:10 +0800

    modify custom configuration

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,5 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Modify hostname
 sed -i 's/OpenWrt/Newifi-D1/g' package/base-files/files/bin/config_generate
 
-
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
